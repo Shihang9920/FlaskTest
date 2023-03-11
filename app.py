463efc4547bff7c3ea5b8260c4eb154de441ca70
Shihang <shihang9920@163.com>
2023-03-11 13:14:41 +0800

    修改

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('blog.html')
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -18,25 +18,16 @@ def login():
         print(name, password)
 
 
-# @app.route('/register', methods=["GET", "POST"])
-# def register():
-#     if request.method == "GET":
-#         return render_template('register.html')
-#     else:
-#         name = request.form.get('username')
-#         password = request.form.get('password')
-#         gender = request.form.get('gender')
-#         language = request.form.getlist('language')
-#         city = request.form.get('city')
-#         message = request.form.get('message')
-#         print(name, password, gender, language, city, message)
-#         return "Done"
-#
-
 @app.route('/getregister')
 def get_register():
     print(request.args)
     return "Done"
+
+
+@app.route('/userlist')
+def showuser():
+    users = ["Tom", "Jerry", "Brain", "Sam"]
+    return render_template('users.html', title="HAHA", data_list=users)
 
 
 if __name__ == '__main__':
